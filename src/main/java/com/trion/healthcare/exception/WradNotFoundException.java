ee57c8fdd61c6ddff5b5292b4b3bc7fa5a2d49e4
Rameshkarukuri <rameshkarukuriram97@gmail.com>
2022-09-02 14:53:51 +0530

    Added new Exception

---
+++ src/main/java/com/trion/healthcare/exception/WradNotFoundException.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package com.trion.healthcare.exception;
+
+/**
+ * @author Ram Karukuri
+ *
+ */
+public class WradNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	public WradNotFoundException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public WradNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public WradNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public WradNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 * @param enableSuppression
+	 * @param writableStackTrace
+	 */
+	public WradNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
